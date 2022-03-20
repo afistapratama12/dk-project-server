90c2c5ae78b60024759bbf9636a86c8e41150e2c
afistapratama12 <pratama.otori.12@gmail.com>
2022-03-20 20:15:31 +0700

    handle bug range week count

--- utils/countRangeDate.go
+++ utils/countRangeDate.go
@@ -15,14 +15,27 @@ func CountRangeDate() (string, string, error) {
 
 	dateRange := int(timeLocal.Weekday())
 
-	start := timeLocal.Add(time.Duration(-dateRange+1) * time.Hour * 24)
-	end := timeLocal.Add(time.Duration(7-dateRange) * time.Hour * 24)
+	if dateRange == 0 {
+		start := timeLocal.Add(-6 * time.Hour * 24)
+		end := timeLocal.Add(time.Duration(dateRange) * time.Hour * 24)
 
-	startDate := time.Date(start.Year(), start.Month(), start.Day(), 0, 0, 0, 0, start.Location())
-	endDate := time.Date(end.Year(), end.Month(), end.Day(), 23, 59, 59, 0, end.Location())
+		startDate := time.Date(start.Year(), start.Month(), start.Day(), 0, 0, 0, 0, start.Location())
+		endDate := time.Date(end.Year(), end.Month(), end.Day(), 23, 59, 59, 0, end.Location())
 
-	startDateStr := startDate.Format(entity.ParseFormat)
-	endDateStr := endDate.Format(entity.ParseFormat)
+		startDateStr := startDate.Format(entity.ParseFormat)
+		endDateStr := endDate.Format(entity.ParseFormat)
 
-	return startDateStr, endDateStr, err
+		return startDateStr, endDateStr, nil
+	} else {
+		start := timeLocal.Add(time.Duration(-dateRange+1) * time.Hour * 24)
+		end := timeLocal.Add(time.Duration(7-dateRange) * time.Hour * 24)
+
+		startDate := time.Date(start.Year(), start.Month(), start.Day(), 0, 0, 0, 0, start.Location())
+		endDate := time.Date(end.Year(), end.Month(), end.Day(), 23, 59, 59, 0, end.Location())
+
+		startDateStr := startDate.Format(entity.ParseFormat)
+		endDateStr := endDate.Format(entity.ParseFormat)
+
+		return startDateStr, endDateStr, err
+	}
 }
